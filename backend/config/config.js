b6ff8ab9dc8ceb198b12b65704c5ec683261f6d9
tmnpierre <tomyn.pierre@gmail.com>
2024-05-27 17:54:32 +0200

    chore(config): update Sequelize configuration and environment variables

--- backend/config/config.js
+++ backend/config/config.js
@@ -16,8 +16,17 @@ module.exports = {
     // Dialecte utilisé par Sequelize pour interagir avec la base de données (ici, PostgreSQL)
     dialect: 'postgres'
   },
-  // Configuration pour l'environnement de production (à compléter en fonction des besoins)
+  // Configuration pour l'environnement de production
   production: {
-    // Ajouter les configurations de production ici
+    // Nom d'utilisateur pour la base de données, récupéré depuis les variables d'environnement
+    username: process.env.DB_USER,
+    // Mot de passe pour la base de données, récupéré depuis les variables d'environnement
+    password: process.env.DB_PASS,
+    // Nom de la base de données, récupéré depuis les variables d'environnement
+    database: process.env.DB_NAME,
+    // Hôte de la base de données, récupéré depuis les variables d'environnement ou 'localhost' par défaut
+    host: process.env.DB_HOST || 'localhost',
+    // Dialecte utilisé par Sequelize pour interagir avec la base de données (ici, PostgreSQL)
+    dialect: 'postgres'
   }
 };
